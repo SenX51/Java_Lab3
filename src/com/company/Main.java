cb4208a0249033ddb3f1f39676f502bd19244550
SenX51 <Arsemenov51@yandex.ru>
2022-04-17 19:00:12 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+// Семенов Арсений, 3 курс, 62 группа, 3 лабораторная
+// Сравнить производительность ArrayList и LinkedList
+package com.company;
+
+import java.util.Scanner;
+import  java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
